e4036d33ba0f4909a21b5c879b0e3a1c8c17bbd4
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-08 17:06:29 +0100

    lodash demo

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
 // Code goes here!
+import _ from 'lodash';
+
+// * npm i @types/lodash - to get the types for this js library. Allows you to work with it in TS.
+
+console.log(_.shuffle([1, 2, 3]));
+
+// * `declare` keyword - 
